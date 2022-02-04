719b68af1bb5e8031e4d1998a018fe2b6cc49647
Anthony Alexander Montemuro <75276447+tony-montemuro@users.noreply.github.com>
2022-02-03 18:11:07 -0600

    Create two-sum.cpp

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //Brute force method
+        bool isFound = false;
+        std::vector<int> v(2);
+        int i, j;
+        for (i = 0; i < nums.size()-1; i++) {
+            for (j = i+1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    v[0] = i, v[1] = j;
+                    isFound = true;
+                    break;
+                }
+            }
+            if (isFound) break;
+        }
+        return v;
+    }
+};
